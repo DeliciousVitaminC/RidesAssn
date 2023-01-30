0cc3d5d3d2236a28a08cbd667d78f270545eea5a
zhaohe <a1320555916@outlook.com>
2023-01-30 09:54:17 -0400

    Project Init

---
+++ Rides/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Rides
+//
+//  Created by zhaohe on 2023-01-30.
+//
+
+import Foundation
+
+struct Constants {
+    static let appName = "Rides"
+    static let requestURL = "https://random-data-api.com/api/vehicle/random_vehicle?size="
+}
