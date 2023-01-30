9493b42691cb2450a291ce84277c9dd8d2cad502
zhaohe <a1320555916@outlook.com>
2023-01-30 11:20:06 -0400

    Completed input textfield and search button

--- Rides/View/MainViewController.swift
+++ Rides/View/MainViewController.swift
@@ -9,15 +9,45 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchTextField.delegate = self
+        searchTextField.keyboardType = .numberPad
+        searchTextField.addDoneButtonOnKeyBoardWithControl()
         
         tableView.layer.cornerRadius = 25
         
     }
 
+}
 
+extension MainViewController : UITextFieldDelegate {
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if searchTextField.text != "" {
+            print("TODO: Replace with actual query" + searchTextField!.text!)
+            searchTextField.text = ""
+        }
+    }
+}
+
+extension UITextField {
+    func addDoneButtonOnKeyBoardWithControl() {
+        let keyboardToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: safeAreaLayoutGuide.layoutFrame.width, height: 44))
+        keyboardToolbar.sizeToFit()
+        keyboardToolbar.barStyle = .default
+        let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneBarButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.endEditing(_:)))
+        keyboardToolbar.items = [flexBarButton, doneBarButton]
+        self.inputAccessoryView = keyboardToolbar
+    }
 }
 
